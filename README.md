0b27b6e4ca2377b7db8e42d21164633256d1323c
NASEEM A P <naseemalassampattil@gmail.com>
2025-04-14 16:34:00 +0530

    added setup.py, updated requirements.txt, updated README -> added installation

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Deeplearning-MLOps
+
+
+## How to run ?
+### Steps:
+
+Clone the repository
+```bash
+git clone https://github.com/naseemap47/Deeplearning-MLOps
+```
+### Step 01 - Create conda env
+```bash
+cd Deeplearning-MLOps/
+conda create -n mlops python=3.10 -y
+```
+
+### Step 01 - Install the requirements
+```bash
+conda activate mlops
+sudo apt install libpq-dev python3-dev
+pip install -r requirements.txt
+```
+
---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Deeplearning-MLOps"
+AUTHER_USER_NAME = "naseemap47"
+SRC_REPO = "cnnClassifier"
+AUTHER_EMAIL = "naseemalassampattil@gmail.com"
+
+setuptools.setup(
+    name=REPO_NAME,
+    version=__version__,
+    author=AUTHER_USER_NAME,
+    author_email=AUTHER_EMAIL,
+    description="A small python package for CNN App",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHER_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHER_USER_NAME}/{REPO_NAME}/issues"
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
